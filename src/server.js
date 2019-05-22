4da8a972a4c3ba507864d2022011f3f77357fdb0
AdrianM <30712077+amichot@users.noreply.github.com>
2019-05-22 16:05:10 -0500

    added articles and comments tables and tests

---
+++ src/server.js
@@ -0,0 +1,16 @@
+//dependencies
+const knex = require('knex');
+//components
+const app = require('./app');
+const {PORT, DB_URL} = require('./config');
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL,
+});
+
+app.set('db', db);
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
